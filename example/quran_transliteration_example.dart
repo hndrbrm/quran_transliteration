0f7a5dcb396a0c3beb640ea08b3ddfc2b7933ac4
Eyem <eyem@Eyems-MacBook-Air.local>
2022-11-07 00:46:50 +0700

    Initial Files.

---
+++ example/quran_transliteration_example.dart
@@ -0,0 +1,6 @@
+import 'package:quran_transliteration/quran_transliteration.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/quran_transliteration.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library quran_transliteration;
+
+export 'src/quran_transliteration_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
